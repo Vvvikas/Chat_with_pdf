dfbaea15d41dab87e8c9f0dbb22e9eab899a6d40
Vvvikas <vikassrawat5@gmail.com>
2026-08-04 12:10:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,18 @@ if uploaded_file:
         data = uploaded_file.read()
     st.sidebar.pdf(data)
 
-#============STEP 4: ===============#
+# =================STEP 4: LOAD RESOURCES=================
+@st.cache_data
+def load_documents():
+    loader = PyPDFLoader(uploaded_file)
+    documents = loader.load()
+    return documents
+
+# st.cache_data: to load data only one time
+# st.cache_resource : to load resource only one time
+
+@st.cache_resource
+def load_embedding():
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return embeddings|
+
